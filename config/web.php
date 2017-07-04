efc925d985092f17256b90f633fa2763b402bd56
devkam <nam-kam@mail.ru>
2017-07-04 14:11:18 +0400

    Uptade

--- config/web.php
+++ config/web.php
@@ -27,6 +27,7 @@ $config = [
         'user' => [
             'identityClass' => 'app\models\User',
             'enableAutoLogin' => true,
+            'loginUrl' => 'site/login',
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
--- modules/admin/controllers/AppAdminController.php
+++ modules/admin/controllers/AppAdminController.php
@@ -1,15 +1,30 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: devkam
- * Date: 03.07.17
- * Time: 17:44
+ * User: Andrey
+ * Date: 15.05.2016
+ * Time: 15:53
  */
 
 namespace app\modules\admin\controllers;
+use yii\web\Controller;
+use yii\filters\AccessControl;
 
 
-class AppAdminController
-{
+class AppAdminController extends Controller{
+
+    public function behaviors(){
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@']
+                    ]
+                ]
+            ]
+        ];
+    }
 
 }
